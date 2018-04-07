cc794d6b047f2c3b8d5fbf6b00927d55b544238e
Joshua Schipull <joshschipull@gmail.com>
2018-04-07 14:42:20 -0500

    Add project files.

---
+++ Converters/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Converters.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Converters.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
